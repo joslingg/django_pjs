7050a55fb8c067a5a2776c6fa3a531f3187f9023
joslingg <truonghoanglinh787@gmail.com>
2025-05-22 15:42:58 +0700

    Tinh chỉnh FE, import full data

--- doanvien_site/doanvien_site/settings.py
+++ doanvien_site/doanvien_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-omjm)@af0=#wgu8*&0!1-21ocdnr-ynd7s$lviz22yt(ov@7bd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
